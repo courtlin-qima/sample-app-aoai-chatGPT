bdffcf2219691880f2c2da3283c93765d0264860
Francois Noyez <francois.noyez@qima.com>
2024-08-08 11:37:57 +0200

    WIP chore: remove implementation of citation title and page nb data display for now

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -782,12 +782,16 @@ const Chat = () => {
                                 <span aria-label="Citations" className={styles.citationPanelHeader}>Citations</span>
                                 <IconButton iconProps={{ iconName: 'Cancel' }} aria-label="Close citations panel" onClick={() => setIsCitationPanelOpen(false)} />
                             </Stack>
+                            {/*
                             <h5 className={styles.citationPanelTitle} tabIndex={0} title={activeCitation.url && !activeCitation.url.includes("blob.core") ? activeCitation.url : activeCitation.title ?? ""} onClick={() => onViewSource(activeCitation)}>Title:</h5>
                             <div>{activeCitation.title ? activeCitation.title : "<undefined>"}</div>
+                            */}
                             <h6 className={styles.citationPanelTitle} tabIndex={0} >Original document name:</h6>
                             <div>{activeCitation.filepath ? activeCitation.filepath.split("/").pop() : "<undefined>"}</div>
+                            {/*
                             <h6 className={styles.citationPanelTitle} tabIndex={0} >Original document page:</h6>
                             <div>{activeCitation.page ? activeCitation.page : "<undefined>"}</div>
+                            */}
                             <h6 className={styles.citationPanelTitle} tabIndex={0} >Content:</h6>
                             <div tabIndex={0}>
                                 <ReactMarkdown
